145ab0de079d977966b0d0e69e226227d1026d20
Bjorn Wallner <bjornwallner@gmail.com>
2017-05-24 15:01:11 +0200

    added complex.pdb to test

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
 import pickle
 import os
+import sys
+path_to_script=os.path.abspath(sys.arv[0])
+
 with open("cutoffs", 'rb') as f:
     cutoffs = pickle.load(f)
 
@@ -11,6 +14,6 @@ print cutoffs
 print freq
 for c in cutoffs:
     for f in freq:
-        cmd="./pymol_clusters.py -c {} -f {} test/avgpsn test/complex.pdb".format(c,f)
+        cmd="{}/pymol_clusters.py -c {} -f {} {}test/avgpsn {}test/complex.pdb".format(path_to_script,c,f,path_to_script,path_to_script)
         print cmd
-        os.system(cmd)
+        #os.system(cmd)
